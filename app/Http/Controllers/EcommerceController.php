58a044569585e8ebdf92fb2e8400a635d863131a
Soeng Souy <soengsouy.khmer09@gmail.com>
2024-03-31 22:19:06 +0700

    update create product

--- app/Http/Controllers/EcommerceController.php
+++ app/Http/Controllers/EcommerceController.php
@@ -4,12 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\CategoryProduction;
+use App\Models\ColorsVariant;
+use App\Models\Gender;
+use App\Models\ProductType;
+use App\Models\Size;
+use App\Models\StatusProduct;
+use App\Models\Visibility;
+use App\Models\TAXApplicable;
+
 class EcommerceController extends Controller
 {
     /** page create product */
     public function productCreate()
     {
-        return view('ecommerce.product-create');
+        $CategoryProduction = CategoryProduction::all();
+        $Gender             = Gender::all();
+        $ProductType        = ProductType::all();
+        $Size               = Size::all();
+        $StatusProduct      = StatusProduct::all();
+        $Visibility         = Visibility::all();
+        $TAXApplicable      = TAXApplicable::all();
+        return view('ecommerce.product-create',compact('CategoryProduction','Gender','ProductType','Size','StatusProduct','Visibility','TAXApplicable'));
     }
 
     /** page product overview */
